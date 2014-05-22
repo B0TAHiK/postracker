84d7692cb0e8db946d1c77d3803293003c8504f8
greg2010 <greg2008200@gmail.com>
2014-05-22 04:59:17 +0400

    corp API info added to the table

--- cron/apiValidate.cron.php
+++ cron/apiValidate.cron.php
@@ -13,19 +13,29 @@ $result = mysql_query($query);
 $keyIDarr = array();
 $vCodearr = array();
 $maskarr = array();
+$corparr = array();
 while($row = mysql_fetch_assoc($result)){
     $keyIDarr[] = $row[keyID];
     $vCodearr[] = $row[vCode];
     $maskarr[] = $row[mask];
+    $corparr[] = $row[corporation];
 }
 for ($k = 0; $k < count($keyIDarr); $k++) {
     $keyID = $keyIDarr[$k];
     $vCode = $vCodearr[$k];
     $mask = $maskarr[$k];
+    $corp = $corparr[$k];
 //    Making API request...
     $maskAPI = get_mask($keyID, $vCode);
+    $page = "https://api.eveonline.com/account/apiKeyInfo.xml.aspx";
+    $api = api_req($page, $keyID, $vCode, '', '', '', '');
+    $corporationName = $api->result->key->rowset->row->attributes()->corporationName;
     if ($mask != $maskAPI) {
         $query = "UPDATE `apilist` SET `mask` = '$maskAPI' WHERE `keyID` = '$keyID'";
+        $result = mysql_query($query);
+    }
+    if ($corp != $corporationName) {
+        $query = "UPDATE `apilist` SET `corporation` = '$corporationName' WHERE `keyID` = '$keyID'";
         $result = mysql_query($query);
     }
 }
